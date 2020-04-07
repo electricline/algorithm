66f13768f57ee491d270c0498c32d5ddcf2597fa
electricline <jktmobile6637@gmail.com>
2020-04-07 23:34:07 +0900

    프로그래머스 타겟 넘버(DFS)

---
+++ 프로그래머스-타겟넘버/targetNumber.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class targetNumber {
+    static class Solution {
+        static int answer = 0;
+        public int solution(int[] numbers, int target) {
+            dfs(numbers,target,0);
+
+            return answer;
+        }
+
+
+         void dfs(int[] numbers, int target, int depth){
+            if(depth == numbers.length){
+                int sum=0;
+                for(int num: numbers){
+                    sum += num;
+                }
+                if(sum == target){
+                    answer++;
+                }
+            } else {
+                numbers[depth] *= 1;
+                dfs(numbers,target, depth+1);
+                System.out.println("Zz");
+                numbers[depth]*=-1;
+                dfs(numbers,target,depth+1);
+            }
+        }
+    }
+
+
+}
