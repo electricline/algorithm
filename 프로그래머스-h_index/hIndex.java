479678f01a2e96f55a4899700f6bdd6847138233
electricline <jktmobile6637@gmail.com>
2020-04-12 15:12:01 +0900

    ㅍ프로그래머스

---
+++ 프로그래머스-h_index/hIndex.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class hIndex {
+    static class Solution {
+        public int solution(int[] citations) {
+            int answer = 0;
+            int cite;
+            Arrays.sort(citations);
+            for(int i=0; i<citations.length; i++) {
+                cite = citations.length - i;
+
+                if(citations[i]>=cite){
+                    answer = cite;
+                    break;
+                }
+
+
+            }
+
+            return answer;
+        }
+    }
+}
