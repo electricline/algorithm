4ffb0ec10ebb9869720f5e1a2ab3463ad7895c72
electricline <jktmobile6637@gmail.com>
2020-04-17 13:52:38 +0900

    프로그래머스-큰 수 만들기

---
+++ 프로그래머스-큰수만들기/makeBigNumber.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.HashSet;
+
+public class makeBigNumber {
+    static class Solution {
+        public String solution(String number, int k) {
+
+            StringBuilder stringBuilder = new StringBuilder();
+            int idx = -1;
+            char max;
+            for(int i=0; i< number.length()-k; i++){
+                max = '0';
+                for(int j= idx+1; j<=i+k; j++){
+                    if(max < number.charAt(j)){
+                        max = number.charAt(j);
+                        idx = j;
+                    }
+                }
+                stringBuilder.append(max);
+            }
+            return stringBuilder.toString();
+        }
+    }
+}
