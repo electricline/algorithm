443dfcf38edb253bc8172d537d04c03bc0a4632c
electricline <jktmobile6637@gmail.com>
2020-12-23 18:35:31 +0900

    20201223 leetcode brute force and one pass

--- leetcode/src/com/company/MergeTwoSortedLists.java
+++ leetcode/src/com/company/MergeTwoSortedLists.java
@@ -11,7 +11,7 @@ public class MergeTwoSortedLists {
           ListNode(int val) { this.val = val; }
           ListNode(int val, ListNode next) { this.val = val; this.next = next; }
           }
-          
+
     class Solution {
             public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
