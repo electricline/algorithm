9c93f9a0b0fb67fd8d3e177c46678bc282e82d47
electricline <jktmobile6637@gmail.com>
2020-12-27 00:54:42 +0900

    20201227 dp(x)

--- leetcode/src/com/company/MaximumPointsYouCanObtainfromCards.java
+++ leetcode/src/com/company/MaximumPointsYouCanObtainfromCards.java
@@ -12,11 +12,9 @@ public class MaximumPointsYouCanObtainfromCards {
 
             int maxScore = result;
 
-            int[] dp = new int[k+1];
-            dp[0] = maxScore;
             for(int i=1; i<=k; i++){
-                dp[i] = dp[i-1] + cardPoints[i - 1] - cardPoints[cardPoints.length - k + i - 1];
-                maxScore = Math.max(maxScore, dp[i]);
+                result = result + cardPoints[i - 1] - cardPoints[cardPoints.length - k + i - 1];
+                maxScore = Math.max(maxScore, result);
             }
 
             return maxScore;
