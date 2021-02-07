721d30c34be480c29f17b429b0df56328f8b4406
electricline <jktmobile6637@gmail.com>
2021-02-07 16:52:05 +0900

    boj, 2750, success, 정렬

---
+++ boj/src/boj2750.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class boj2750 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(j, j + 1, arr);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+
+    private static void swap(int a, int b, int[] arr) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+}
