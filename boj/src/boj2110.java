b3d72bab4ec990fff248662572c30338cf27db93
electricline <jktmobile6637@gmail.com>
2021-02-14 14:55:19 +0900

    boj, 2110 , success, 이분탐색

--- boj/src/boj2110.java
+++ boj/src/boj2110.java
@@ -9,6 +9,7 @@ public class boj2110 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken()); int c = Integer.parseInt(st.nextToken());
         int[] arr = new int[n];
