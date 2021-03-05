efea069f3e31338a224ca0a93ee47f382260d838
electricline <jktmobile6637@gmail.com>
2021-03-05 20:55:00 +0900

    boj, n과 m 8, success , 백트래킹

---
+++ boj/src/boj15657.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class boj15657 {
+
+    static int n, m;
+    static int[] arr;
+    static boolean[] visitied;
+    static List<Integer> list = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        visitied = new boolean[n];
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth, int idx) {
+        if(depth == m){
+            for(int i : list){
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=idx; i<n; i++){
+            list.add(arr[i]);
+            dfs(depth+1, i);
+            list.remove(list.size()-1);
+        }
+
+    }
+}
