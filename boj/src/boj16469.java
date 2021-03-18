15040d3b6e79ec0ca08ae74f492dc4100355d2f2
electricline <jktmobile6637@gmail.com>
2021-03-18 20:00:16 +0900

    boj, 16469, success , bfs

--- boj/src/boj16469.java
+++ boj/src/boj16469.java
@@ -15,7 +15,7 @@ public class boj16469 {
     static int[] dx = {0, 0, -1, 1};
     static int min = Integer.MAX_VALUE;
     static int res = -1;
-    static int[][][] visiteid;
+    static int[][][] visitied;
 
     public static void main(String[] args) throws IOException {
 
@@ -26,7 +26,7 @@ public class boj16469 {
 
 
         map = new int[r + 1][c + 1];
-        visiteid = new int[r + 1][c + 1][3];
+        visitied = new int[r + 1][c + 1][3];
 
 
 
@@ -37,9 +37,9 @@ public class boj16469 {
             }
         }
 
-        for(int[][] first :  visiteid){
+        for(int[][] first : visitied){
             for(int[] second : first){
-                Arrays.fill(second,0);
+                Arrays.fill(second, - 1);
             }
         }
 
@@ -55,27 +55,23 @@ public class boj16469 {
         c_c = Integer.parseInt(st.nextToken());
 
 
-
-        solve();
-
+        for(int i=0; i<3; i++){
+            solve(i);
+        }
 
         for (int i = 1; i <= r; i++) {
             for (int j = 1; j <= c; j++) {
-
-                if(visiteid[i][j][0] != 0 && visiteid[i][j][1] != 0 && visiteid[i][j][2] != 0){
-
-                    int temp = Math.max(visiteid[i][j][0], visiteid[i][j][1]);
-                    temp = Math.max(temp,visiteid[i][j][2]);
-
+                if(visitied[i][j][0] != -1 && visitied[i][j][1] != -1 && visitied[i][j][2] != -1 && map[i][j] == 0){
+                    int temp = Math.max(visitied[i][j][0], visitied[i][j][1]);
+                    temp = Math.max(temp,visitied[i][j][2]);
+                    if(temp == -1) continue;
                     if(min > temp){
                         min = temp;
                         res = 1;
                     } else if(min == temp){
                         res++;
                     }
-
                 }
-
             }
         }
 
@@ -89,27 +85,41 @@ public class boj16469 {
 
     }
 
-    private static void solve() {
+    private static void solve(int idx) {
 
         Queue<pair> q = new LinkedList<>();
-        q.add(new pair(a_r, a_c, 0));
-        q.add(new pair(b_r, b_c, 1));
-        q.add(new pair(c_r, c_c, 2));
+
+        if(idx == 0){
+            q.add(new pair(a_r, a_c, 0,0));
+        } else if(idx == 1){
+            q.add(new pair(b_r, b_c, 1,0));
+        } else {
+            q.add(new pair(c_r, c_c, 2,0));
+        }
+
+
+        visitied[a_r][a_c][0] = 0;
+        visitied[b_r][b_c][1] = 0;
+        visitied[c_r][c_c][2] = 0;
+
 
         while (!q.isEmpty()) {
 
             pair cur = q.poll();
+//            System.out.println(cur.r + " " + cur.c);
 
-            for (int i = 0; i < 4; i++) {
+            for(int i=0; i<4; i++){
 
                 int nr = cur.r + dy[i];
                 int nc = cur.c + dx[i];
-                if(nr < 1 || nr > r || nc < 1 || nc > c) continue;
-                if(map[nr][nc] == 1) continue;
-                if(visiteid[nr][nc][cur.number] != 0) continue;
-                visiteid[nr][nc][cur.number] = visiteid[cur.r][cur.c][cur.number] + 1;
-                q.add(new pair(nr, nc, cur.number));
+
+                if(nr >=1 && nr <= r && nc >= 1 && nc <= c && map[nr][nc] == 0 && visitied[nr][nc][cur.number] == -1){
+                    visitied[nr][nc][cur.number] = cur.score + 1;
+                    q.add(new pair(nr, nc, cur.number, cur.score+1));
+                }
+
             }
+
         }
 
     }
@@ -119,11 +129,13 @@ public class boj16469 {
         int r;
         int c;
         int number;
+        int score;
 
-        pair(int r, int c, int number) {
+        pair(int r, int c, int number, int score) {
             this.r = r;
             this.c = c;
             this.number = number;
+            this.score = score;
         }
     }
 
