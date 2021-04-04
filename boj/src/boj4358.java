cec3ec70588c2ec392c99c0facb7b8d41e8972e0
hI <jktmobile6637@gmail.com>
2021-04-04 14:54:51 +0900

    boj, 4358 , success , hash + pq

--- boj/src/boj4358.java
+++ boj/src/boj4358.java
@@ -1,2 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class boj4358 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<String, Integer> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        double treeNums = 0;
+        while(true){
+
+            String treeName = br.readLine();
+            if(treeName == null || treeName.length() == 0)
+                break;
+
+            map.put(treeName, map.getOrDefault(treeName, 0) + 1);
+            treeNums++;
+        }
+
+        PriorityQueue<String> pq = new PriorityQueue();
+        for(String key  : map.keySet()){
+            pq.add(key);
+        }
+
+        while(!pq.isEmpty()){
+
+            String name = pq.poll();
+            String ratios = String.format("%.4f", map.get(name) * 100 / treeNums);
+            sb.append(name).append(" ").append(ratios).append('\n');
+
+        }
+
+        System.out.println(sb);
+
+    }
+
 }
